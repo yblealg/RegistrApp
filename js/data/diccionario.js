aeaadb401c72ef75605dc832022dc728825317a1
yblealg <bernardoguerra1980@gmail.com>
2026-05-15 09:10:36 -0500

    js carpeta

---
+++ js/data/diccionario.js
@@ -0,0 +1,179 @@
+export const DICCIONARIO_SIR = {
+
+    "1":{
+        acto:"VENTA",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "2":{
+        acto:"HIPOTECA",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "7":{
+        acto:"DIVISION MATERIAL",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "8":{
+        acto:"CANCELACION HIPOTECA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "9":{
+        acto:"PATRIMONIO DE FAMILIA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "10":{
+        acto:"EMBARGO",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "12":{
+        acto:"REPRODUCCION CONSTANCIA",
+        tarifa:"FIJA",
+        folios:"NO",
+        valor:17500
+    },
+
+    "14":{
+        acto:"PROPIEDAD HORIZONTAL",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "19":{
+        acto:"MATRICULAS",
+        tarifa:"FIJA",
+        folios:"NO",
+        valor:15300
+    },
+
+    "21":{
+        acto:"ENGLOBE",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "27":{
+        acto:"DESENGLOBE",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "35":{
+        acto:"DECLARACION CONSTRUCCION",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "38":{
+        acto:"VIVIENDA VIS",
+        tarifa:"ESPECIAL",
+        folios:"NO"
+    },
+
+    "43":{
+        acto:"DECLARACION CONSTRUCCION SIN CUANTIA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "70":{
+        acto:"CANCELACION HIPOTECA SIN CUANTIA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "84":{
+        acto:"ARRENDAMIENTO",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "98":{
+        acto:"LAUDO ARBITRAL",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "99":{
+        acto:"INSCRIPCION FOLIO ADICIONAL",
+        tarifa:"FIJA",
+        folios:"NO",
+        valor:15300
+    },
+
+    "103":{
+        acto:"DONACION",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "120":{
+        acto:"LIQUIDACION SOCIEDAD CONYUGAL",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "156":{
+        acto:"EXPROPIACION",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    },
+
+    "200":{
+        acto:"ACTUALIZACION NOMENCLATURA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "300":{
+        acto:"ESTAMPILLA",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "619":{
+        acto:"POSESION REGULAR LEY 1183",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "888":{
+        acto:"OTROS",
+        tarifa:"MANUAL",
+        folios:"NO"
+    },
+
+    "903":{
+        acto:"ACTUALIZACION AREA Y LINDEROS",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "906":{
+        acto:"CAMBIO DE NOMBRE",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "907":{
+        acto:"CAMBIO RAZON SOCIAL",
+        tarifa:"SIN CUANTIA",
+        folios:"SI"
+    },
+
+    "1013":{
+        acto:"TRANSFERENCIA DOMINIO CESION",
+        tarifa:"CON CUANTIA",
+        folios:"NO"
+    }
+};
